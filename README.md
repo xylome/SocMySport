4f5f1c54eee5539ecb527f96ea9036e8d79d7228
Xavier Héroult <xavier.heroult@orange.com>
2019-03-22 15:23:47 +0100

    First public version

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # SocMySport
-Add your weekly cycled distance to your twitter name
+Python script to add your weekly cycled distance (from strava) to your twitter name.
+
+## Dependencies
+This script should be run with the version 3 of Python, with these libraries:
+* configparser
+* requests
+* BeautifulSoup
+* twitter
+
+Install them with:
+```
+pip3 install "library_name"
+```
+## Prerequisites
+You have to create a twitter developper account and get your own consumer_key and consumer_secret credentials. You can follow the process described on the twurl page to do so : https://github.com/twitter/twurl#getting-started.
+
+## Installation
+Grab a copy of the config file in the example folder and place it under
+```
+$HOME/.config/SocMySport/config.txt
+```
+Set the variables to real values.
+
+The *name* variable will be parsed to replace &lt;distance&gt; by your weekly cycled distance.
+
+## Run
+Execute:
+```
+python3 SocMySport.py
+```
+## Automate
+Add the above command to your crontab :)
---
+++ socMySport.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+__author__ = "Xavier Héroult <xavier@placard.fr.eu.org>"
+__copyright__ = "Copyright 2019, Xavier Héroult"
+__credits__ = ["Xavier Héroult"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Xavier Héroult"
+__email__ = "xavier@placard.fr.eu.org"
+__status__ = "Prototype"
+
+import os
+from os.path import expanduser
+
+import configparser
+import requests
+from bs4 import BeautifulSoup
+
+import twitter
+
+home = expanduser("~")
+CONF_FILE = home + "/.config/SocMySport/config.txt"
+
+strava_cookie_name = '_strava4_session'
+headers = {
+    'User-agent' : 'SocMySport ' + __version__,
+    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding' : 'gzip, deflate, br',
+    'Accept-Language' : 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+}
+cookies = {
+    strava_cookie_name : ""
+}
+
+try:
+    conf_stat = os.stat(CONF_FILE)
+except FileNotFoundError as e:
+    print("Fichier de conf", CONF_FILE, "introuvable. --", e)
+    exit(1)
+
+try:
+    config = configparser.RawConfigParser()
+except BaseException as e:
+    print("fichier pas au bon format.", e)
+    exit(2)
+
+try:
+    config.read(CONF_FILE)
+except:
+    print("fichier pas au bon format")
+    exit(3)
+
+try:
+    name = config.get('general', 'name')
+    consumer_key = config.get("twitter", "consumer_key")
+    consumer_secret = config.get("twitter", "consumer_secret")
+    access_token_key = config.get("twitter", "access_token_key")
+    access_token_secret = config.get("twitter", "access_token_secret")
+    strava_login = config.get("strava", "login")
+    strava_password = config.get("strava", "password")
+except BaseException as e:
+    print("il manque des choses dans la conf, vérifiez.", e)
+    exit(4)
+
+login = requests.get('https://www.strava.com/login', headers=headers)
+html = login.text
+doc = BeautifulSoup(html, features='lxml')
+
+element = doc.find('meta', attrs={'name':'csrf-token'})
+if element.attrs['content'] != "":
+    authenticity_token = element.attrs['content']
+else:
+    print("No authenticity_token found, exiting.")
+    exit(10)
+
+for cookie in login.cookies:
+    cookies[strava_cookie_name] = cookie.value
+
+headers['Referer'] = 'https://www.strava.com/login'
+
+payload = {
+    'authenticity_token' : authenticity_token,
+    'utf8' : '✓',
+    'plan' : '',
+    'email': strava_login,
+    'password' : strava_password,
+    'remember_me': 'on'
+}
+
+session = requests.post('https://www.strava.com/session', data=payload, headers=headers, cookies=cookies)
+for cookie in session.cookies:
+    if cookie.name == strava_cookie_name:
+        cookies[strava_cookie_name] = cookie.value
+
+doc = BeautifulSoup(session.text, features='lxml')
+for element in doc.find('span', attrs={'class':'actual'}):
+    distance = element
+
+new_name = name.replace("<distance>", distance)
+print("Will set new name on twitter:", new_name)
+
+api = twitter.Api(consumer_key = consumer_key, consumer_secret = consumer_secret,
+                    access_token_key = access_token_key, access_token_secret = access_token_secret)
+
+api.UpdateProfile(name=new_name)
+
+print("Done.")
